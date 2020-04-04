5a13c6afee368c8ff368cb339f6dd6419bd06b12
Nathan <nateim3@gmail.com>
2020-04-04 10:36:28 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Buffer<double> buffer = new Buffer<double>();
+
+            ProcessInput(buffer);
+            ProcessBuffer(buffer);
+        }
+
+        private static void ProcessBuffer(IBuffer<double> buffer)
+        {
+            double sum = 0.0;
+            Console.WriteLine("Buffer : ");
+            while (!buffer.IsEmpty)
+            {
+                sum += buffer.Read();
+            }
+            Console.WriteLine(sum);
+        }
+
+        private static void ProcessInput(IBuffer<double> buffer)
+        {
+            while (true)
+            {
+                double value = 0.0;
+                string input = Console.ReadLine();
+                
+                if (double.TryParse(input, out value))
+                {
+                    buffer.Write(value);
+                    continue;
+                }
+                break;
+            }
+        }
+    }
+}
